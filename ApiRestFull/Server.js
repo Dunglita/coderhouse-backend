70666ffc01795174e1e1e2f26b986c88996c71ea
Dani <garciadanos@gmail.com>
2022-03-10 23:52:39 -0300

    Created main files

--- ApiRestFull/Server.js
+++ ApiRestFull/Server.js
@@ -1,29 +1,67 @@
 const express = require("express");
+const Router = express;
 const Contenedor = require("./Contenedor");
 const contenedor = new Contenedor("./productos.txt");
 
 const app = express();
+const router = Router();
 
 const port = 8080;
+
+router.use(express.json()); //Se podria ahorrar usando body-parser
 
 const server = app.listen(port, () => {
   console.log(`Server listening on port: ${port}`);
 });
 
-app.get("/productos", async (req, res) => {
-  res.type("json");
-  res.send(
-    JSON.stringify(await contenedor.getAll(), null, 2) +
-      `\n \nLink en glitch: \nhttps://ripe-witty-cobweb.glitch.me/productos`
-  );
+server.on("error", (err) => "There was an error starting the server" + { err });
+
+app.use("/api/productos", router);
+
+app.use(express.static("public"));
+app.use("static", express.static("public"));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname, "/index.html");
 });
 
-app.get("/productoRandom", async (req, res) => {
-  let products = await contenedor.getAll();
-  let indice = Math.floor(Math.random() * products.length);
+router.get("/", async (req, res) => {
   res.type("json");
-  res.send(
-    JSON.stringify(products[indice], null, 2) +
-      `\n \nLink en glitch: \nhttps://ripe-witty-cobweb.glitch.me/productorandom`
-  );
+  res.send(JSON.stringify(await contenedor.getAll(), null, 2));
+});
+
+router.get("/:id", async (req, res) => {
+  let product = await contenedor.getById(parseInt(req.query.id));
+  res.type("json");
+  res.send(JSON.stringify(product, null, 2));
+});
+
+router.post("/", async (req, res) => {
+  console.log(req.query);
+  const producto = {
+    title: req.query.title,
+    price: req.query.price,
+    thumbnail: req.query.thumbnail,
+  };
+  let product = await contenedor.create(producto);
+  res.type("json");
+  res.send(JSON.stringify(product, null, 2));
+});
+
+router.put("/:id", async (req, res) => {
+  const producto = {
+    title: req.body.title,
+    price: req.body.price,
+    thumbnail: req.body.thumbnail,
+    id: parseInt(req.params.id),
+  };
+  let product = await contenedor.update(producto);
+  res.type("json");
+  res.send(JSON.stringify(product, null, 2));
+});
+
+router.delete("/:id", async (req, res) => {
+  let messege = await contenedor.deleteById(parseInt(req.params.id));
+  res.type("json");
+  res.send(JSON.stringify(messege, null, 2));
 });
