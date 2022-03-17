c20492ec4e15ebd5b2c882681c5b12f8b37c4d5b
Dani <garciadanos@gmail.com>
2022-03-17 19:34:46 -0300

    Created a server file for each template engines

---
+++ TemplatesEngines/Pug/Server.js
@@ -0,0 +1,58 @@
+const fs = require("fs");
+const express = require("express");
+const Router = express;
+const Contenedor = require("../Contenedor");
+const contenedor = new Contenedor("./productos.txt");
+
+var bodyParser = require("body-parser");
+
+const app = express();
+const router = Router();
+
+const port = 8080;
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+router.use(express.json()); //Se podria ahorrar usando body-parser
+
+const server = app.listen(port, () => {
+  console.log(`Server listening on port: ${port}`);
+});
+
+server.on("error", (err) => "There was an error starting the server" + { err });
+
+app.use("/api/productos", router);
+
+app.use(express.static("public"));
+app.use("static", express.static("public"));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname, "/index.html");
+});
+
+router.get("/", async (req, res) => {
+  res.type("json");
+  res.send(JSON.stringify(await contenedor.getAll(), null, 2));
+});
+
+router.get("/:id", async (req, res) => {
+  let product = await contenedor.getById(parseInt(req.query.id));
+  res.type("json");
+  res.send(JSON.stringify(product, null, 2));
+});
+
+router.post("/", async (req, res) => {
+  console.log(req.body);
+  const producto = {
+    title: req.body.title,
+    price: req.body.price,
+    thumbnail: req.body.thumbnail,
+  };
+  let product = await contenedor.create(producto);
+  res.type("json");
+  res.send(JSON.stringify(product, null, 2));
+});
