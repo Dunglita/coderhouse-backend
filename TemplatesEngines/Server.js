3f4c4b8780f04e706949488deedd952e3c58fd5c
Dani <garciadanos@gmail.com>
2022-03-17 22:21:55 -0300

    Created server file

---
+++ TemplatesEngines/Server.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const Contenedor = require("./Contenedor");
+const handlebars = require("express-handlebars");
+const contenedor = new Contenedor("./productos.txt");
+const app = express();
+
+const port = 8080;
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.json());
+
+app.use(express.static("public"));
+
+const server = app.listen(port, () => {
+  console.log(`Server listening on port: ${port}`);
+});
+
+server.on("error", (err) => "There was an error starting the server" + { err });
+
+app.engine(
+  "hbs",
+  handlebars({
+    extname: ".hbs",
+    defaultLayout: "index.hbs",
+    layoyutsDir: __dirname + "/views/layouts",
+  })
+);
+
+app.set("view engine", "hbs");
+
+app.set("views", "./views");
+
+const productos = [];
+
+app.get("/", (req, res) => {
+  res.render("form");
+});
+
+app.post("/productos", async (req, res) => {
+  const producto = {
+    title: req.body.title,
+    price: req.body.price,
+    thumbnail: req.body.thumbnail,
+  };
+  let product = await contenedor.create(producto);
+  res.render("form");
+});
+
+app.get("/productos", async (req, res) => {
+  let product = JSON.stringify(await contenedor.getAll(), null, 2);
+  res.render("table");
+});
