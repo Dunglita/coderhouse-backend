e97d68572c9efc1f967a97be37f6f4841e6a76ff
Dani <garciadanos@gmail.com>
2022-03-29 23:55:51 -0300

    Created main client file

--- Websockets/Public/Js/Main.js
+++ Websockets/Public/Js/Main.js
@@ -1,5 +1,35 @@
 const socket = io.connect();
-socket.on("mi mensaje", (data) => {
-  alert(data);
-  socket.emit("notificacion", "Mensaje recibido exitosamente");
+
+const title = document.querySelector("#title");
+const price = document.querySelector("#price");
+const thumbnail = document.querySelector("#thumbnail");
+
+document.querySelector("button").addEventListener("click", () => {
+  socket.emit("newProduct", {
+    title: title.value,
+    price: price.value,
+    thumbnail: thumbnail.value,
+  });
 });
+
+socket.on("mensajes", (msjs) => {
+  const mensajesHTML = msjs
+    .map((msj) => `SocketId: ${msj.socketid} -> Mensaje: ${msj.mensaje}`)
+    .join("<br>");
+  document.querySelector("p").innerHTML = mensajesHTML;
+});
+
+socket.on("productList", (productList) => {
+  console.table(productList);
+  makeHtmlTable(productList).then((html) => {
+    document.getElementById("table_container").innerHTML = html;
+  });
+});
+
+async function makeHtmlTable(productList) {
+  const respuesta = await fetch("views/table.hbs");
+  const plantilla = await respuesta.text();
+  const template = compile(plantilla);
+  const html = template({ productList });
+  return html;
+}
